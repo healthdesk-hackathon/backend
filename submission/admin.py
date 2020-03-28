057294a8083cd9604f834b1f2ed1e2c63e0cf994
Phil <phil.ayres@consected.com>
2020-03-28 14:35:16 +0000

    Select a chosen medical center in the submission

--- submission/admin.py
+++ submission/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+import hashlib
+import uuid
 
-# Register your models here.
+from django.db import models
+
+class MedicalCenter(models.Model):
+    """
+    Available medical centers, from which a patient can choose.
+
+    This is as limited set of attributes, to reflect what is needed for a simple, 
+    GPS located submission.
+
+    Addtional address information could be added in the future if necessary.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=50, blank=False)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
