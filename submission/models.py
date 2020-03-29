2695790a0531f774a172be76dfc4b5b838eacfc8
Phil <phil.ayres@consected.com>
2020-03-29 23:26:06 +0100

    Test

--- submission/models.py
+++ submission/models.py
@@ -26,17 +26,14 @@ class Submission(models.Model):
     insurance number, phone number, email, etc
     """
 
-    class Severity(models.TextChoices):
-        MINOR = 'MIN', 'Minor'
-        MEDIUM = 'MED', 'Medium'
-        MAJOR = 'MAJ', 'Major'
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     identifier = models.CharField(max_length=50, null=False)
     id_type = models.CharField(max_length=50, null=False)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True, related_name='submissions')
-    is_admitted = models.BooleanField(null=True, blank=True)
-    severity = models.CharField(max_length=3, choices=Severity.choices, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     @property
     def patient_anon_id(self):
